ca6c23a2aa969f5431855ca43e04594d670b561c
thanseert5529 <thanseerthanu5529@gmail.com>
2023-05-28 15:47:17 +0530

    escape character revert into \ and path issue solved by regex(utils)

--- es_runner/dsl_generator/utils.js
+++ es_runner/dsl_generator/utils.js
@@ -1,19 +1,21 @@
 function prepareQueryFilter(query) {
-  return query
-          .replace(/index\s*=/g, '_index=')
-           .replace(/\s*>\s*([^=])/g,':>$1')
-           .replace(/\s*<\s*([^=])/g,':<$1')
-           .replace(/\s*>=\s*/g,':>=')
-           .replace(/\s*<=\s*/g,':<=')
-           .replace(/\s*(\w+)\s*!=\s*/g,' NOT $1:')
-           .replace(/([^><])=/g,'$1:')
-           .replace(/\\/g, "\\\\");
-
+  const query_arr = query.split("=");
+  if (query_arr[1]?.includes("####"))
+  query=`${query_arr[0]}="${query_arr[1]}"`.replace(/####/g, "\\\\") 
+    return query
+      .replace(/index\s*=/g, "_index=")
+      .replace(/\s*>\s*([^=])/g, ":>$1")
+      .replace(/\s*<\s*([^=])/g, ":<$1")
+      .replace(/\s*>=\s*/g, ":>=")
+      .replace(/\s*<=\s*/g, ":<=")
+      .replace(/\s*(\w+)\s*!=\s*/g, " NOT $1:")
+      .replace(/([^><])=/g, "$1:");
 }
+
 module.exports = {
   prepareQueryFilter /* : (q)=>{
     const c = prepareQueryFilter(q)
     console.log(c)
     return c
-  }  */
-}
+  }  */,
+};
