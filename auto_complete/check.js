601d9f5788f3de9deac55354faead68e52a9db7f
thanseert5529 <thanseerthanu5529@gmail.com>
2023-05-31 16:16:10 +0530

    new check script

--- auto_complete/check.js
+++ auto_complete/check.js
@@ -4,6 +4,16 @@ const { parse } = require('../grammar')
 
 async function check(query) {
   try {
+    const query_arr=query.split("=")
+    if(query_arr[2]!==undefined)
+{
+    if(query_arr[2].includes("\\"))
+    {
+        query=query_arr[0]+"="+query_arr[1]+"="+(query_arr[2].replace(/\\/g,"####").replace(/"/g,""))
+    
+    }
+}
+
     parse(query)
     return {
       error: false
