422935ea476cc73d6e4a9c72e080cc2bcc520de4
thanseert5529 <thanseerthanu5529@gmail.com>
2023-06-05 18:35:54 +0530

    add excape character for : / in path

--- es_runner/dsl_generator/utils.js
+++ es_runner/dsl_generator/utils.js
@@ -2,18 +2,18 @@ function prepareQueryFilter(query) {
   const new_path_regex = /!@#+((?!!@#).)+!@#/g;
 
   paths = query.match(new_path_regex);
-  if(paths)
-  for (const path of paths) {
-    const new_path = path
-      .replace(/####/g, "\\\\")
-      .replace(/:\\\\/g, "\\:\\\\")
-      .replace(/ +/g, "*")
-      .replace(/!@#/g, "")
-      .replace(/\(|\)/g, "*");
-    query = query.replace(path, new_path)
-  }
+  if (paths)
+    for (const path of paths) {
+      const new_path = path
+        .replace(/####/g, "\\\\")
+        .replace(/:/g, "\\:")
+        .replace(/\//g, "\\/")
+        .replace(/ +/g, "*")
+        .replace(/!@#/g, "")
+        .replace(/\(|\)/g, "*");
+      query = query.replace(path, new_path);
+    }
   return query
-  
     .replace(/index\s*=/g, "_index=")
     .replace(/\s*>\s*([^=])/g, ":>$1")
     .replace(/\s*<\s*([^=])/g, ":<$1")
