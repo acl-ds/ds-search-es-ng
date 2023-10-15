6dc2cd73beb172a3f7ee6e736be8376f4baa1ad2
thanseer5529 <thanseerthanu5529@gmail.com>
2023-10-15 15:58:45 +0530

    worker_threadless searching

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ const suggest = require("./auto_complete");
 
 const check = require("./auto_complete/check");
 
-const { Client } = require("@elastic/elasticsearch");
 
-const pgp = require("pg-promise");
 
 const q_seq = require("./query_sequencer/index");
 
