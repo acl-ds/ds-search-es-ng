d57a4ad72ec8a1b54f7e7ca023567918f6fe6f2d
thanseer5529 <thanseerthanu5529@gmail.com>
2024-04-18 14:11:40 +0530

    query transformation before grammer logic changed

--- query_sequencer/index.js
+++ query_sequencer/index.js
@@ -42,15 +42,28 @@ async function executeQuery(query, timePicker, options) {
   const parsedQuery = [];
   let parseTook = 0;
   try {    
-    // const path_regex =/""+((?!"").)+""/g;
-    // let paths = query.match(path_regex)    
-    // if(paths)    
-    // for (const path of paths)
-    // {
-    //   const new_path = path.replace(/""/g, "!@#").replace(/\\/g,"####");
-    //   query = query.replace(path, new_path);
-    // }
-    query = query.replace(/"/g, "!@#").replace(/\\/g,"####");
+    const path_regex = /"".*?""/g;
+    const rvrs_query = query.split("").reverse().join("");
+    const new_path = query.match(path_regex);
+    let rvrs_new_path = rvrs_query.match(path_regex);
+    if (new_path && rvrs_new_path) {
+      rvrs_new_path = rvrs_new_path.map((item) => {
+        return item.split("").reverse().join("");
+      });
+      for (i = 0; i < new_path.length; i++) {
+        let path;
+        if (new_path[i] !== rvrs_new_path[i]) {
+          path = `${new_path[i].slice(
+            0,
+            new_path[i].length / 2 + 1
+          )}${rvrs_new_path[i].slice(rvrs_new_path[i].length / 2)}`;
+        } else path = new_path[i];
+        query = query.replace(
+          path,
+          path.replace(/"/g, "!@#").replace(/\\/g, "####")
+        );
+      }
+    }
     console.log(query);
     const parseStart = performance.now();
     const res = parser.parse(query);
