e3645b7343e39802c04cdf2f3f3b50c51d88e8f8
thanseer5529 <thanseerthanu5529@gmail.com>
2024-09-11 11:09:13 +0530

    used more accurate regex for matching != in query

--- es_runner/dsl_generator/utils.js
+++ es_runner/dsl_generator/utils.js
@@ -17,7 +17,8 @@ function prepareQueryFilter(query) {
     .replace(/\s*<\s*([^=])/g, ":<$1")
     .replace(/\s*>=\s*/g, ":>=")
     .replace(/\s*<=\s*/g, ":<=")
-    .replace(/\s*(\w.+)\s*!=\s*/g, " NOT $1:")
+    // .replace(/\s*(\w.+)\s*!=\s*/g, " NOT $1:")
+    .replace(/(\S+)\s*!=\s*([^ ]+)/g, 'NOT $1:$2')
     .replace(/([^><])=/g, "$1:")
     .replace(/!@#/g, '\\\"');
 }
