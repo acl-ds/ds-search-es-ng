926b72a2c12110bce9c7bb285175498a5c45a2e0
thanseer5529 <thanseerthanu5529@gmail.com>
2024-09-23 21:28:06 +0530

    preprocessor of search ng parser optimized

--- query_sequencer/index.js
+++ query_sequencer/index.js
@@ -53,10 +53,15 @@ async function executeQuery(query, timePicker, options) {
       for (i = 0; i < new_path.length; i++) {
         let path;
         if (new_path[i] !== rvrs_new_path[rvrs_new_path.length-i-1]) {
-          path = `${new_path[i].slice(
-            0,
-            new_path[i].length / 2 + 1
-          )}${rvrs_new_path[rvrs_new_path.length-i-1].slice(rvrs_new_path[rvrs_new_path.length-i-1].length / 2)}`;
+          const  rvrs_new_path_length=rvrs_new_path[rvrs_new_path.length - i - 1].length;
+          const new_path_length=new_path[i].length
+          if(rvrs_new_path_length===new_path_length) 
+            {
+                path = `${new_path[i].slice(0,new_path[i].length / 2 + 1)}${rvrs_new_path[rvrs_new_path.length - i - 1].slice(rvrs_new_path[rvrs_new_path.length - i - 1].length / 2)}`;
+            }
+            else {
+             path=new_path_length>rvrs_new_path_length?new_path[i]:rvrs_new_path[i]
+            }
         } else path = new_path[i];
         query = query.replace(
           path,
